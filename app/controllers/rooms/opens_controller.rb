60b14fbcf331b4a3401ea238ca3dd3494157c6b3
siavashmohammadi <siavash.mohammadi@flexiana.com>
2026-08-26 18:41:03 +0330

    feature: add room plus botton on project sidebar

--- app/controllers/rooms/opens_controller.rb
+++ app/controllers/rooms/opens_controller.rb
@@ -4,6 +4,7 @@ class Rooms::OpensController < RoomsController
   before_action :remember_last_room_visited, only: :show
   before_action :force_room_type, only: %i[ edit update ]
   before_action :ensure_permission_to_create_rooms, only: %i[ new create ]
+  before_action :set_project_for_new_room, only: %i[ new create ]
 
   DEFAULT_ROOM_NAME = "New room"
 
@@ -12,13 +13,16 @@ class Rooms::OpensController < RoomsController
   end
 
   def new
-    @room = Rooms::Open.new(name: DEFAULT_ROOM_NAME)
+    @room = Rooms::Open.new(name: DEFAULT_ROOM_NAME, project: @project)
     @users = User.active.ordered
     @agents = Agent.active
   end
 
   def create
-    room = Rooms::Open.create_for(room_params, users: Current.user)
+    room_attributes = room_params
+    room_attributes[:project_id] = @project.id if @project
+
+    room = Rooms::Open.create_for(room_attributes, users: Current.user)
 
     broadcast_create_room(room)
     redirect_to room_url(room)
@@ -37,6 +41,16 @@ class Rooms::OpensController < RoomsController
   end
 
   private
+    def set_project_for_new_room
+      project_id = params[:project_id] || params.dig(:room, :project_id)
+      return if project_id.blank?
+
+      @project = Current.user.projects.find_by(id: project_id)
+      return if @project
+
+      redirect_to root_url, alert: "Project not found or inaccessible"
+    end
+
     # Allows us to edit a closed room and turn it into an open one on saving.
     def force_room_type
       @room = @room.becomes!(Rooms::Open)
