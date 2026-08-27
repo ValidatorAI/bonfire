be1dc427cfe2d4b21f162e935f932d48b9ddd1b7
siavashmohammadi <siavash.mohammadi@flexiana.com>
2026-08-27 12:42:11 +0330

    list of user and bots are filtered based on the project

--- app/controllers/rooms/settings_controller.rb
+++ app/controllers/rooms/settings_controller.rb
@@ -8,8 +8,8 @@ module Rooms
       @users = @room.users.without_bots.ordered
       @bot_users = @room.users.active_bots.ordered
       @bots = @room.agents.ordered
-      @available_users = User.active.without_bots.ordered.where.not(id: @users.select(:id))
-      @available_bots = available_bots_scope.where.not(id: @bots.select(:id))
+      @available_users = available_users_scope.where.not(id: @users.select(:id))
+      @available_ai_teammate_options = available_ai_teammate_options
 
       render layout: false
     end
@@ -37,7 +37,7 @@ module Rooms
       end
 
       def add_user
-        user = User.active.find_by(id: params[:participant_id])
+        user = available_users_scope.find_by(id: params[:participant_id])
         return if user.blank?
 
         @room.memberships.grant_to(user)
@@ -54,10 +54,10 @@ module Rooms
       end
 
       def add_agent
-        agent = available_bots_scope.find_by(id: params[:participant_id])
-        return if agent.blank?
+        participant = find_available_ai_participant(params[:participant_id])
+        return if participant.blank?
 
-        @room.memberships.grant_to(agent)
+        @room.memberships.grant_to(participant)
       end
 
       def remove_agent
@@ -75,7 +75,46 @@ module Rooms
         @room.project ? @room.project.agents.ordered : Agent.none
       end
 
-      alias_method :available_bots_scope, :available_agents_scope
+      def available_users_scope
+        @room.project ? @room.project.users.active.without_bots.ordered : User.none
+      end
+
+      def available_bot_users_scope
+        @room.project ? @room.project.users.active_bots.ordered : User.none
+      end
+
+      def available_ai_teammate_options
+        available_agent_options + available_bot_user_options
+      end
+
+      def available_agent_options
+        available_agents_scope.where.not(id: @bots.select(:id)).map do |agent|
+          [ "#{agent.name} AI", "Agent:#{agent.id}" ]
+        end
+      end
+
+      def available_bot_user_options
+        available_bot_users_scope.where.not(id: @bot_users.select(:id)).map do |bot_user|
+          [ "#{bot_user.name} AI", "User:#{bot_user.id}" ]
+        end
+      end
+
+      def find_available_ai_participant(participant_key)
+        key = participant_key.to_s
+
+        # Backward compatibility with legacy numeric values (Agent ids only)
+        return available_agents_scope.find_by(id: key) if key.match?(/\A\d+\z/)
+
+        type, id = key.split(":", 2)
+        return if id.blank?
+
+        case type
+        when "Agent"
+          available_agents_scope.find_by(id: id)
+        when "User"
+          available_bot_users_scope.find_by(id: id)
+        end
+      end
 
       def broadcast_room_added_for(user)
         html = render_to_string(partial: "users/sidebars/rooms/shared", locals: { room: @room })
