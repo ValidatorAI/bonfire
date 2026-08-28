bb5604e6b91758562c9db230e83fc6c1fb04805a
siavashmohammadi <siavash.mohammadi@flexiana.com>
2026-08-28 09:21:41 +0330

    correct bot crud redirect issue

--- app/controllers/accounts/bots/keys_controller.rb
+++ app/controllers/accounts/bots/keys_controller.rb
@@ -3,6 +3,11 @@ class Accounts::Bots::KeysController < ApplicationController
 
   def update
     User.active_bots.find(params[:bot_id]).reset_bot_key
-    redirect_to account_bots_url
+    redirect_to account_bots_url(return_to: return_to_param)
   end
+
+  private
+    def return_to_param
+      "company_settings" if params[:return_to] == "company_settings"
+    end
 end
