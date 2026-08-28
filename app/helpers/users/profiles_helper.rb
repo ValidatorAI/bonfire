6d8d79208ce9951a3ff47a2f754a3b3d8d182ee5
siavashmohammadi <siavash.mohammadi@flexiana.com>
2026-08-28 17:35:02 +0330

    correct btn submit button

--- app/helpers/users/profiles_helper.rb
+++ app/helpers/users/profiles_helper.rb
@@ -8,7 +8,7 @@ module Users::ProfilesHelper
   end
 
   def profile_form_submit_button
-    tag.button class: "btn btn--reversed center txt-large", type: "submit" do
+    tag.button class: "btn btn--reversed center bg-white txt-large", type: "submit" do
       image_tag("check.svg", aria: { hidden: "true" }, size: 20) +
       tag.span("Save changes", class: "for-screen-reader")
     end
