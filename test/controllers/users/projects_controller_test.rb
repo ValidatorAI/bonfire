af835e61607cf76d1819e86118cabe641d20e7a8
siavashmohammadi <siavash.mohammadi@flexiana.com>
2026-08-31 10:33:20 +0330

    Knowledge page: rendering from db for section 🏛️ Decision Records

--- test/controllers/users/projects_controller_test.rb
+++ test/controllers/users/projects_controller_test.rb
@@ -407,6 +407,7 @@ class Users::ProjectsControllerTest < ActionDispatch::IntegrationTest
     assert_no_match "Networked Notes (Obsidian Sync)", @response.body
     assert_no_match "obsidian-container", @response.body
     assert_match "No files or directories found in this project.", @response.body
+    assert_match "No decision records found in this project.", @response.body
   end
 
   test "knowledge_file serves markdown as json" do
